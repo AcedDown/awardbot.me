785097d43f40b12e1591becdcbe999d93e6b1cc8
Jake H <73356928+AcedDown@users.noreply.github.com>
2022-05-07 21:45:16 +0000

    Update login.jsx

--- pages/api/auth/login.jsx
+++ pages/api/auth/login.jsx
@@ -1,6 +1,6 @@
 export default function Login(req, res) {
     res.redirect(
-        'https://joshuawithoutua.award.repl.co/v1/auth/login?__b=' + 
+        'https://api.lunyx.me/v1/auth/login?__b=' + 
         (process.env.NODE_ENV === 'production' ? 'false' : 'true') + 
         '&url=' + (req.query.url || '/')
     );
