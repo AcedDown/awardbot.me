5ea02260160e45e855cd6682a2441403e58437d7
Jake H <73356928+AcedDown@users.noreply.github.com>
2022-11-19 19:40:42 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-  const withPWA = require("next-pwa");
-
-
+  const withPWA = require('next-pwa')({
   dest: 'public'
+  // disable: process.env.NODE_ENV === 'development',
+  // register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  //...
+})
 
 module.exports = withPWA({
 
