214d68fd95a3ffbb3d6a0f38c7c6c63e9b7cdc3b
Jake H <73356928+AcedDown@users.noreply.github.com>
2022-11-19 20:03:16 +0000

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -8,11 +8,11 @@ export default function Privacy({ $ }) {
             <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Award</h1>
             <p className="text-sm text-white/30 mb-4">Last Update: 01/01/2022</p>
 
-            <p className={textClasses}>At Award, accessible from https://awardbot.me, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Award and how we use it.</p>
+            <p className={textClasses}>At Lunyx, accessible from https://lunyx.me, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Lunyx and how we use it.</p>
 
             <p className={textClasses}>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.</p>
 
-            <p className={textClasses}>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Award. This policy is not applicable to any information collected offline or via channels other than this website.</p>
+            <p className={textClasses}>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Lunyx. This policy is not applicable to any information collected offline or via channels other than this website.</p>
 
             <h2 className={titleClasses}>Consent</h2>
 
@@ -40,25 +40,25 @@ export default function Privacy({ $ }) {
 
             <h2 className={titleClasses}>Log Files</h2>
 
-            <p className={textClasses}>Award follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p>
+            <p className={textClasses}>Lunyx follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p>
 
             <h2 className={titleClasses}>Cookies and Web Beacons</h2>
 
-            <p className={textClasses}>Like any other website, Award uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.</p>
+            <p className={textClasses}>Like any other website, Lunyx uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.</p>
 
 
 
             <h2 className={titleClasses}>Advertising Partners Privacy Policies</h2>
 
-            <p className={textClasses}>You may consult this list to find the Privacy Policy for each of the advertising partners of Award.</p>
+            <p className={textClasses}>You may consult this list to find the Privacy Policy for each of the advertising partners of Lunyx.</p>
 
-            <p className={textClasses}>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Award, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
+            <p className={textClasses}>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Lunyx, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
 
-            <p className={textClasses}>Note that Award has no access to or control over these cookies that are used by third-party advertisers.</p>
+            <p className={textClasses}>Note that Lunyx has no access to or control over these cookies that are used by third-party advertisers.</p>
 
             <h2 className={titleClasses}>Third Party Privacy Policies</h2>
 
-            <p className={textClasses}>Award's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. </p>
+            <p className={textClasses}>Lunyx's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. </p>
 
             <p className={textClasses}>You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.</p>
 
@@ -85,7 +85,7 @@ export default function Privacy({ $ }) {
 
             <p className={textClasses}>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
 
-            <p className={textClasses}>Award does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
+            <p className={textClasses}>Lunyx does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
         </div>
     );
 };
