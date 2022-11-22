a05cc91f12b12199c5bb9bc59fe11bf333d68182
Jake H <73356928+AcedDown@users.noreply.github.com>
2022-11-22 15:29:51 +0000

    Update Create.jsx

--- components/Panel/Create.jsx
+++ components/Panel/Create.jsx
@@ -95,11 +95,11 @@ export default function Create({ isOpen, closeModal, mutateList }) {
                     <span className="text-xs text-zinc-400 ml-2">Banner URL:</span>
                     <input value={banner} onChange={e => setBanner(e.target.value)} placeholder="https://..." type="text" className="mb-2 text-white bg-black/10 py-2 px-4 rounded-xl focus:outline-none w-full border border-white/10 focus:border-amber-400 focus:text-amber-400 transform duration-200" spellCheck={false} autoComplete={false} />
                     <span className="text-xs text-zinc-400 ml-2">Title:</span>
-                    <input value={title} onChange={e => setTitle(e.target.value)} placeholder="Award" type="text" className="mb-2 text-white bg-black/10 py-2 px-4 rounded-xl focus:outline-none w-full border border-white/10 focus:border-amber-400 focus:text-amber-400 transform duration-200" spellCheck={false} autoComplete={false} />
+                    <input value={title} onChange={e => setTitle(e.target.value)} placeholder="Lunyx" type="text" className="mb-2 text-white bg-black/10 py-2 px-4 rounded-xl focus:outline-none w-full border border-white/10 focus:border-amber-400 focus:text-amber-400 transform duration-200" spellCheck={false} autoComplete={false} />
                     <span className="text-xs text-zinc-400 ml-2">Description:</span>
-                    <input value={desc} onChange={e => setDesc(e.target.value)} placeholder="Lorem ipsum dolor sit amet." type="text" className="mb-2 text-white bg-black/10 py-2 px-4 rounded-xl focus:outline-none w-full border border-white/10 focus:border-amber-400 focus:text-amber-400 transform duration-200" spellCheck={false} autoComplete={false} />
+                    <input value={desc} onChange={e => setDesc(e.target.value)} placeholder="This is a description to be displayed." type="text" className="mb-2 text-white bg-black/10 py-2 px-4 rounded-xl focus:outline-none w-full border border-white/10 focus:border-amber-400 focus:text-amber-400 transform duration-200" spellCheck={false} autoComplete={false} />
                     <span className="text-xs text-zinc-400 ml-2">Website URL:</span>
-                    <input value={url} onChange={e => setUrl(e.target.value)} placeholder="https://awardbot.me" type="text" className="text-white bg-black/10 py-2 px-4 rounded-xl focus:outline-none w-full border border-white/10 focus:border-amber-400 focus:text-amber-400 transform duration-200" spellCheck={false} autoComplete={false} />
+                    <input value={url} onChange={e => setUrl(e.target.value)} placeholder="https://lunyx.me" type="text" className="text-white bg-black/10 py-2 px-4 rounded-xl focus:outline-none w-full border border-white/10 focus:border-amber-400 focus:text-amber-400 transform duration-200" spellCheck={false} autoComplete={false} />
                 </div>
 
                 <div className="mt-4 flex items-center space-x-2">
